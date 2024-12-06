07157d64638063d8e319777ee0bdb3ba94b52b60
Pierre Lahaye <pierre.lahaye@etu.u-bordeaux.fr>
2024-12-06 02:27:03 +0100

    app.vue components/Header.vue assets/main.css maj header ajout de la possibilité de cliquer sur dive quest et remonter en haut

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,9 @@
     <header
         class="fixed top-0 left-0 right-0 h-16 flex items-center justify-center z-50"
     >
-        <NuxtLink to="/"> Dive Quest </NuxtLink>
+        <NuxtLink to="/" v-if="$route.path != '/'"> Dive Quest </NuxtLink>
+        <a href="#" v-else> Dive Quest </a>
+
     </header>
 </template>
 
