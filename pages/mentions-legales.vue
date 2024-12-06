d400006f4a32ed84dbb71b320d7b3dc075d22065
untypequicode <ethan.facca@untypequicode.fr>
2024-12-06 07:46:43 +0100

    pages/contact.vue pages/mentions-legales.vue switch entre contact en mensions légales

--- pages/mentions-legales.vue
+++ pages/mentions-legales.vue
@@ -1,51 +1,53 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <h1 class="mt-4">Mentions légales</h1>
-    <p class="text-center text-xl font-bold text-red-500 mt-4">
-        Pas de plagiat ici !
-    </p>
-    <h2>
-        Réaliser durant
+    <div class="h-20"></div>
+    <p class="ml-6">
         <a
-            href="https://www.nuitdelinfo.com/"
+            href="https://untypequicode.fr/"
+            target="_blank"
+            class="text-blue-500 underline"
+            >Ethan Facca</a
+        >
+    </p>
+    <p class="ml-6">
+        <a
+            href="https://divequest.untypequicode.fr/contact"
             target="_blank"
             class="text-blue-500 underline"
         >
-            la nuit de l'info 2024
+            Autolien
         </a>
-    </h2>
-    <h3>
-        Par:
-        <p class="ml-6">
-            -<a
-                href="https://untypequicode.fr/"
-                target="_blank"
-                class="text-blue-500 underline"
-            >
-                Ethan FACCA</a
-            >
-            <span class="text-sm ml-2">(Le LEAD de la team !)</span>
-        </p>
-        <p class="ml-6">
-            - Vincent BRUNEAU
-            <span class="text-sm ml-2"></span>
-        </p>
-        <p class="ml-6">
-            - Pierre LAHAYE
-            <span class="text-sm ml-2"></span>
-        </p>
-        <p class="ml-6">
-            - Micky MARI
-            <span class="text-sm ml-2"></span>
-        </p>
-        <p class="ml-6">
-            - Guillaume RENAUD                <NuxtLink class="ml-16" to="/game"><span style="color: white">Game</span></NuxtLink>
-            <span class="text-sm ml-2"></span>
-        </p>
-    </h3>
-    <h4>Hébergeur: infomaniaque</h4>
+    </p>
+    <p class="ml-6">
+        <a
+            href="https://github.com/les-optimciens-du-web/divequest"
+            target="_blank"
+            class="text-blue-500 underline"
+        >
+            Github du repository
+        </a>
+    </p>
+    <p class="ml-6">
+        <a
+            href="https://github.com/les-optimciens-du-web"
+            target="_blank"
+            class="text-blue-500 underline"
+        >
+            Github de l'organisation
+        </a>
+    </p>
 </template>
 
-<script></script>
+<style scoped>
+p {
+    align-self: center;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    text-align: center;
+    font-size: 3em;
+}
+</style>
