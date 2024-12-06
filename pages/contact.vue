12749677b08d94ad860fdb2ce85d521a2985030d
Vincent BRUNEAU <vincent.bruneau@etu.u-bordeaux.fr>
2024-12-06 06:59:42 +0100

    maj contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,12 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
     <div class="h-20"></div>
     <h1 class="top-20">
-        Contact
-        <NuxtLink to="/game"> Game </NuxtLink>
+        Contact<p class="ml-6">
+      <a href="https://untypequicode.fr/"
+        target="_blank"
+         class="text-blue-500 underline">Ethan Facca</a></p>
+      <p class="ml-6"><a href="https://divequest.untypequicode.fr/contact"
+         target="_blank"
+         class="text-blue-500 underline"> Lien internet </a></p>
+      <p class="ml-6"><a href="https://github.com/les-optimciens-du-web/divequest"
+                         target="_blank"
+                         class="text-blue-500 underline"> Github du repository </a></p>
+      <p class="ml-6"><a href="https://github.com/les-optimciens-du-web"
+                         target="_blank"
+                         class="text-blue-500 underline"> Github de l'organisation </a></p>
     </h1>
-    class
 </template>
 
-<style scoped></style>
+<style scoped>
+p{
+  align-self: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+
+}
+</style>
